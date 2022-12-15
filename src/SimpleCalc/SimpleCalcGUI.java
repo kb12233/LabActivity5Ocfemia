c6bb928da39da68b8fd63c8c68ec693ab4c33cc9
kb12233 <kaybeeocfemia@gmail.com>
2022-12-15 14:05:01 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,8 +1,11 @@
 package SimpleCalc;
 
-import LeapYear.LeapYearGUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
@@ -12,6 +15,72 @@ public class SimpleCalcGUI extends JFrame {
     private JComboBox cbOperations;
     private JButton btnCompute;
 
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                compute();
+            }
+        });
+
+        tfNumber1.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    compute();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+
+        tfNumber2.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    compute();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+
+        cbOperations.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    compute();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
+
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
         app.setTitle("Simple Calculator");
@@ -19,5 +88,26 @@ public class SimpleCalcGUI extends JFrame {
         app.setSize(500, 400);
         app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         app.setVisible(true);
+    }
+
+    public void compute() {
+        int num1 = Integer.parseInt(tfNumber1.getText());
+        int num2 = Integer.parseInt(tfNumber2.getText());
+        int result = 0;
+        switch (cbOperations.getSelectedItem().toString()) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+        }
+        lblResult.setText(String.valueOf(result));
     }
 }
